cce630b38fbf8f2677b92ca9e307af49793a1ea1
xanedesu <xanedesu@gmail.com>
2022-06-21 16:12:04 +0300

    Валидация переменных окружения

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import Joi from 'joi';
 import { AuthModule } from './auth/auth.module';
 import { ContactModule } from './contact/contact.module';
 import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [EventEmitterModule.forRoot(), AuthModule, ContactModule, TagModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_URL: Joi.string().uri().required(),
+        PORT: Joi.number().default(8080),
+        SECRET: Joi.string().required(),
+        AUTHORIZATION_URL: Joi.string().uri().required(),
+      }),
+    }),
+    EventEmitterModule.forRoot(),
+    AuthModule,
+    ContactModule,
+    TagModule,
+  ],
   controllers: [],
   providers: [
     {
