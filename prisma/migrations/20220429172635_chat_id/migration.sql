214a3440b10d2fae145553629fa4ea6013d8198f
xanedesu <xanedesu@gmail.com>
2022-10-19 11:23:13 +0300

    Удалил миграции

--- prisma/migrations/20220429172635_chat_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[chatId]` on the table `Contact` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Contact_chatId_key" ON "Contact"("chatId");
--- prisma/migrations/20220605211555_updated_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Contact" ALTER COLUMN "updatedAt" DROP NOT NULL;
